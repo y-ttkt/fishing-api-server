23fcd782a135406d5c6d6ec3cfcf006b67181383
YusukeTakatsu <github ys.taka44@gmail.com>
2025-02-08 22:00:45 +0900

    feat:add login route

--- cmd/fishing-api-server/main.go
+++ cmd/fishing-api-server/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 	"github.com/yusuke-takatsu/fishing-api-server/config/database"
+	"github.com/yusuke-takatsu/fishing-api-server/infra/repository/user"
+	"github.com/yusuke-takatsu/fishing-api-server/interface/handler"
+	loginUsecase "github.com/yusuke-takatsu/fishing-api-server/usecase/user"
 	"io"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -29,6 +35,21 @@ func main() {
 	}
 
 	defer db.Close()
+
+	userRepo := user.NewRepository(db)
+	login := loginUsecase.NewLoginUseCase(userRepo)
+	userHandler := handler.NewUserHandler(login)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Post("/api/v1/login", userHandler.Login)
+	if err := http.ListenAndServe(":"+os.Getenv("APP_PORT"), r); err != nil {
+		log.Fatalf("ListenAndServe: %v", err)
+	}
 }
 
 func loadEnv() {
